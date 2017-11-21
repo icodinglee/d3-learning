90a475e41748cedf60bcadcbae967fc28f916f30
icodinglee <Lihk11844@126.com>
2017-11-21 19:57:11 +0800

    force layout

---
+++ data/day8.js
@@ -0,0 +1,27 @@
+var nodes = [ 
+    { name: "XiaMen" },
+    { name: "BeiJing"}, 
+    { name: "XiAn" }, 
+    { name: "HangZhou"},
+    { name: "ShangHai"}, 
+    { name: "QingDao"},
+    { name: "NanJing"},
+    { name: "QueShan"}
+];
+
+var links = [  
+        { source : 'BeiJing'  , target: "XiaMen" } , 
+        { source : 'BeiJing'  , target: "XiAn" } , 
+        { source : 'BeiJing'  , target: "XiaMen" } , 
+        { source : 'BeiJing'  , target: "HangZhou" } , 
+        { source : 'BeiJing'  , target: "ShangHai" } , 
+        { source : 'BeiJing'  , target: "QingDao" } , 
+        { source : 'BeiJing'  , target: "NanJing" }, 
+        { source : 'QueShan'  , target: "XiaMen" } , 
+        { source : 'QueShan'  , target: "XiAn" } , 
+        { source : 'QueShan'  , target: "XiaMen" } , 
+        { source : 'QueShan'  , target: "HangZhou" } , 
+        { source : 'QueShan'  , target: "ShangHai" } , 
+        { source : 'QueShan'  , target: "QingDao" } , 
+        { source : 'QueShan'  , target: "NanJing" } 
+];  
